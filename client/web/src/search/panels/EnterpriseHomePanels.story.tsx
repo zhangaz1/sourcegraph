3f3b1e2bbe37f1e9765e1dff2334ee6b6ce6cabf
Erik Seliger <erikseliger@me.com>
2020-10-26 02:27:27 +0100

    Fix story name

--- client/web/src/search/panels/EnterpriseHomePanels.story.tsx
+++ client/web/src/search/panels/EnterpriseHomePanels.story.tsx
@@ -7,7 +7,7 @@ import { SearchPatternType } from '../../graphql-operations'
 import { storiesOf } from '@storybook/react'
 import { WebStory } from '../../components/WebStory'
 
-const { add } = storiesOf('web/search/panels/HomePanels', module).addParameters({
+const { add } = storiesOf('web/search/panels/EnterpriseHomePanels', module).addParameters({
     design: {
         type: 'figma',
         url: 'https://www.figma.com/file/sPRyyv3nt5h0284nqEuAXE/12192-Sourcegraph-server-page-v1?node-id=255%3A3',
